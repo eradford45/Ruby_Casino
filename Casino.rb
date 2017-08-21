0208070a78ded287412495441ada249594693809
Evan Radford <Eradford45@gmail.com>
2017-08-21 16:38:25 -0600

    menu adjustments

--- Casino.rb
+++ Casino.rb
@@ -9,23 +9,32 @@ require_relative 'Deck'
 class Casino
   attr_accessor :player
   def initialize
+    @players = []
     p 'Welcome To The Ruby Casino'
-    @player = Player.new
+    @player1 = Player.new
+    @players << 1
     menu
   end
-
 
   def menu
     p '1) Play High Low'
     p '2) Play Black Jack'
-    p '3) Quit'
-   #option to display wallet
+    p '3) Play slots'
+    p '4) View Wallet'
+    p '5) Create new player'
+    p '6) new game'
+    p '7) Quit'
     case gets.to_i
     when 1
       HighLow.new(@player)
     when 2
       BlackJack.new(@player)
     when 3
+      Slots.new(@player)
+    when 4
+      p "#{@player.wallet.amount}"
+    when 6
+    when 7
       p 'Thanks for Playing'
       exit
     else
