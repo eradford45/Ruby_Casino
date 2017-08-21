6b2ca471dc36dd01dc3f35e69f8d1a690c9264e0
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-08-21 15:20:29 -0600

    blackjack dealer ace decision

--- Black_Jack.rb
+++ Black_Jack.rb
@@ -33,7 +33,7 @@ class BlackJack
     when "n"
       puts "#{hand1}"
       puts "#{dealer_hand}"
-      final(hand1, dealer_hand)
+      final_hands(hand1, dealer_hand)
     else
       puts "Invalid input"
       hitting(hand, dealer_hand)
@@ -44,42 +44,94 @@ class BlackJack
   def ace(e)
     p 'do you want your ace to equal 11 or 1'
     case gets.strip.to_i
-    when 1
-      e = 1
-    when 11
-      e = 11
-    else 
-      p 'Invalid option'
-      ace(e)
+      when 1
+        e = 1
+      when 11
+        e = 11
+      else
+        p 'Invalid option'
+        ace(e)
     end
   end
 
+  def dealer_ace(hand1, deal_rank)
+    deal1_rank = deal_rank.map do |e|
+      if e == 0
+        @sum = deal_rank.reduce(:+)
+        if (@sum + 11) > 21
+          e = 1
+        else
+          e = 11
+        end
+      else
+        e.to_i
+      end
+    end
+    reveal(hand1, deal1_rank)
+  end
 
-  def final(hand1, dealer_hand)
+
+  def final_hands(hand1, dealer_hand)
     rank = []
     hand1.each_with_index do |ranks, i|
     rank << hand1[i].rank
-    rank = rank.map do |e|
-  if e == 'J'
-    10
-  elsif e == 'K'
-    10
-  elsif e == 'Q'
-    10
-  elsif e == 'A'
-    ace(e)
-  else
-    e.to_i
-    
-  end
-end
+      rank = rank.map do |e|
+        if e == 'J'
+          10
+        elsif e == 'K'
+          10
+        elsif e == 'Q'
+          10
+        elsif e == 'A'
+          ace(e)
+        else
+          e.to_i
+        end
+      end
     end
-    binding.pry
 
-
-   
+    deal_rank = []
+    dealer_hand.each_with_index do |ranks, i|
+    deal_rank << dealer_hand[i].rank
+      deal_rank = deal_rank.map do |e|
+        if e == 'J'
+          10
+        elsif e == 'K'
+          10
+        elsif e == 'Q'
+          10
+        elsif e == 'A'
+          0
+        else
+          e.to_i
+        end
+      end
+    end
+    dealer_ace(rank, deal_rank)
   end
+
+  def reveal(rank, deal_rank)
+    sum1 = rank.reduce(:+)
+    sum2 = deal_rank.reduce(:+)
+
+    puts "The dealer has #{sum2}"
+    puts "You have #{sum1}"
+
+    if sum1 > 21
+      puts "You bust! Better luck next time!"
+    elsif sum2 > 21
+      puts "Dealer bust! You win?"
+    elsif sum1 > sum2
+      puts "You're a winner!"
+    elsif sum2 > sum1
+      puts "You're a looser! Better luck next time."
+    else
+      puts "It was a push"
+    end
+  end
+
+
+
+
+
 end
-
-
-
